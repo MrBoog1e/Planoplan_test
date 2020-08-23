7ea3b071e0d4327cb0977d8d52bc1d602ef76ff3
Dmitry Zbirko <mrboog1e@ya.ru>
2020-08-23 19:26:52 +0300

    Update v1.0

--- pages/locators.py
+++ pages/locators.py
@@ -39,4 +39,33 @@ class LoginPageLocators:
     register_button = (By.CLASS_NAME, 'link__View-sc-1ydrjtx-0.koEghb')
     activation_message = (By.CLASS_NAME, 'modal__Container-sc-1iesfw0-2.iqMAKd')
     button_log_sign = (By.CLASS_NAME, 'button-action__View-sc-1xgnbfo-0.cCelhz')
+    error_email_password = (By.CSS_SELECTOR, 'div[data-test="error-message"]')
+    forgot_password = (By.CSS_SELECTOR, 'button[data-test="button_forgot-pass"]')
+    forgot_button = (By.CLASS_NAME, 'buttonLoader__View-hkgzw7-0.iWHvdP')
+    input_check_your_email = (By.CSS_SELECTOR, 'input[name="code"]')
 
+
+class RegisterPageLocators:
+    error_message_email = (By.CLASS_NAME, 'input__Error-sc-185agi7-5.bjJczZ')
+    button_register = (By.CLASS_NAME, 'buttonLoader__View-hkgzw7-0.beSJaU')
+
+
+class ShopPageLocators:
+    shop_button = (By.CSS_SELECTOR, 'a[data-test="widget__button-store"]')
+    overview_button = (By.CSS_SELECTOR, 'button[data-test="sidebar__button-REVIEW"]')
+    packages_button = (By.CSS_SELECTOR, 'button[data-test="sidebar__button-PACKAGES"]')
+    render_button = (By.CSS_SELECTOR, 'button[data-test="sidebar__button-RENDERS"]')
+    panoramas_button = (By.CSS_SELECTOR, 'button[data-test="sidebar__button-PANORAMAS"]')
+    widget_button = (By.CSS_SELECTOR, 'button[data-test="sidebar__button-WIDGET"]')
+    plans_button = (By.CSS_SELECTOR, 'button[data-test="sidebar__button-PRICING"]')
+    #/packages
+    plus_button = (By.CSS_SELECTOR, 'button[data-test="quantity__add"]')
+    minus_button = (By.CSS_SELECTOR, 'button[data-test="quantity__remove"]')
+    add_to_card = (By.CSS_SELECTOR, 'button[data-test="card__button-in-cart"]')
+    basket_button = (By.CSS_SELECTOR, 'button[data-test="sidebar__cart-button"]')
+    basket_amount_product = (By.CSS_SELECTOR, 'div[data-test="quantity__count"]')
+    remove_product = (By.CSS_SELECTOR, 'button[data-test="cart__item-remove"]')
+    message_empty_bucket = (By.CSS_SELECTOR, 'div[class="cart__Text-qkkyd4-1 gqYXXX"]')
+    #/plans
+    chose_plans_button = (By.CSS_SELECTOR, 'a[data-test="pricing__button-upgrade"]')
+    tariff_name = (By.CSS_SELECTOR, 'div[data-test="tariff_name"]')
